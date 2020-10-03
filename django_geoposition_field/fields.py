39502eeea55d2501ded597305996f29bd42435ae
Miroslav Bendík <miroslav.bendik@gmail.com>
2020-10-03 12:39:39 +0200

    Removed ugettext_lazy

--- django_geoposition_field/fields.py
+++ django_geoposition_field/fields.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.core.validators import ValidationError
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from . import Geoposition
 from .forms import GeopositionField as GeopositionFormField
--- django_geoposition_field/forms.py
+++ django_geoposition_field/forms.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from . import Geoposition
 from .widgets import GeopositionWidget
